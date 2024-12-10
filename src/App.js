61bf623eb46b207ed32114d9648a9cd81f913abc
G <girishtibatina@gmail.com>
2024-12-10 13:41:41 +0200

    removed some text

--- src/App.js
+++ src/App.js
@@ -107,9 +107,9 @@ setToggle(!toggle)
   <br></br>
   <p>Our mission is to deliver personalized, top-quality care to each and every one of our patients. We understand that your pet is a cherished member of your family, and we are committed to treating them with the same love, care, and respect that we would give to our own pets.</p>
   <br></br>
-  <p>Our veterinary team is led by [Michael Smosh], a highly respected and experienced veterinarian with a passion for delivering exceptional care. Our support staff is equally dedicated, with a collective goal of providing outstanding customer service and ensuring that every visit to our clinic is a positive and stress-free experience.</p>
+  <p>Our veterinary team is led by Michael Smosh, a highly respected and experienced veterinarian with a passion for delivering exceptional care. Our support staff is equally dedicated, with a collective goal of providing outstanding customer service and ensuring that every visit to our clinic is a positive and stress-free experience.</p>
   <br></br>
-  <p>At [Riverdale animal clinic], we believe in staying at the forefront of veterinary medicine, and we regularly attend continuing education seminars and workshops to ensure that we are always up-to-date on the latest treatments and technologies.</p>
+  <p>At Riverdale animal clinic, we believe in staying at the forefront of veterinary medicine, and we regularly attend continuing education seminars and workshops to ensure that we are always up-to-date on the latest treatments and technologies.</p>
   <br></br>
   <p>We are proud to be an integral part of the local community, and we are honored to have the trust of so many pet owners. If you are looking for a veterinary partner who truly cares, we invite you to explore our website, meet our team, and discover the difference that exceptional care and compassion can make.</p>
   
