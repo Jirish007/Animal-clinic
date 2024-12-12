9d4e4f7bdeb5881d6351468fe7fff44c70ad565b
G <girishtibatina@gmail.com>
2024-12-12 12:04:02 +0200

    Added modal

--- src/components/News.js
+++ src/components/News.js
@@ -4,53 +4,39 @@ import image40 from './images/dogs.jpg'
 
 import { useState } from 'react';
 function News() {
-    const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-const handleClick1 = () => {
-  
-  setIsOpen1(true);
-};
+  const handleOpen = () => {
+    setIsOpen(true);
+  };
 
-const handleClose1 = () => {
-  setIsOpen1(false);
-};
-const [isOpen2, setIsOpen2] = useState(false);
-  
-    const handleClick2 = () => {
-      setIsOpen2(true);
-    };
-  
-    const handleClose2 = () => {
-      setIsOpen2(false);
-    };
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
 
 
     return(
 <>
 <div className='news_container' id='section5'>
 <h2>PET NEWS</h2>
-<div className='franchise'>
-{isOpen1 && (
-        <div className="popup1">
-          <div className="popup1-content">
-            <span className="close1" onClick={handleClose1}>
-              &times;
-            </span>
-            <p>hvbhvkkjbkjb</p>
+<div>
+{isOpen && (
+        <div className="modal-overlay">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h2>Modal Title</h2>
+              <button className="close-button" onClick={handleClose}>
+                &times;
+              </button>
+            </div>
+            <div className="modal-body">
+              <p>This is the modal content.</p>
+            </div>
           </div>
         </div>
       )}
 
-{isOpen2 && (
-        <div className="popup2">
-          <div className="popup2-content">
-            <span className="close2" onClick={handleClose2}>
-              &times;
-            </span>
-            <p>Popup text goes here...</p>
-          </div>
-        </div>
-      )}
 </div>
 
 
@@ -58,14 +44,14 @@ const [isOpen2, setIsOpen2] = useState(false);
 
 <div className='news_main_articles'>
 <section>
-    <p><img src={image40} onClick={handleClick1}
+    <p><img src={image40} onClick={handleOpen}
         style={{ cursor: 'pointer' }}
 ></img></p>
     <p><h4><a href='#'>While some dogs tend to be</a></h4></p>
     <cite>June 12</cite>
 </section>
 <section>
-<p><img src={image50} onClick={handleClick2}
+<p><img src={image50}
         style={{ cursor: 'pointer' }}></img></p>
    <p> <h4><a href='#'>How to train your pet at home?</a></h4></p>
     <cite>June 13</cite>
