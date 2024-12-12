43a5b858d5c59a4deefd5628732c07428d51727a
G <girishtibatina@gmail.com>
2024-12-12 13:11:23 +0200

    removed lazy load from some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { lazy,Suspense } from 'react';
 import './index.css';
 import Footer from './components/Footer';
+import About from './components/About';
+import Services from './components/Services';
+import Navbar from './components/Navbar';
 //THIS IS THE SECTION FOR LAZY LOADING REACT COMPONENTS AND IMAGES FOR SPEED//
-const Navbar = lazy(()=>import("./components/Navbar"))
-const Services = lazy(()=>import("./components/Services"))
-const About = lazy(()=>import("./components/About"))
+
+
+
 const Form = lazy(()=>import("./components/Form"))
 const News = lazy(()=>import("./components/News"))
 const Prices = lazy(()=>import("./components/Prices"))
@@ -16,20 +19,20 @@ function App() {
   return (
     <>
  <div className='app'>
- <Suspense fallback={<h1>Loading</h1>}>
+ 
 <Navbar/>
-</Suspense>
+
  
  <div className='why_title' id='section2'>
   <h2>Top Services</h2>
  </div>
- <Suspense fallback={<h1>Loading</h1>}>
+ 
 <Services/>
-</Suspense>
 
-<Suspense fallback={<h1>Loading</h1>}>
+
+
 <About/>
-</Suspense>
+
     
   
  
